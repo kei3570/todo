456577c8dc2b16db4d5e9730f257d27fd3441fd3
kei3570 <67131453+kei3570@users.noreply.github.com>
2020-06-26 14:37:50 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,34 +5,43 @@
     el: '#app',
     data: {
       newItem:'',
-      todos:[{
-        title:'task 1',
+      todos: [
+        {
+        title:'task1',
+        isDone: false
+      },{
+        title:'task2',
         isDone: false
       },
       {
-        title:'task 2',
-        isDone: false
-      },
-      {
-        title:'task 3',
+        title:'task3',
         isDone: true
-      },
-    ]
+      }
+
+      ]
     },
     methods:{
-      addItem:function(){
-        var item ={
+      addItem: function(){
+        var item={
           title: this.newItem,
           isDone: false
-        };
+        }
         this.todos.push(item);
-        this.newItem = ''
+        this.newItem = '';
       },
       deleteItem: function(index){
         if(confirm('are you sure?')){
-          this.todos.splice(index, 1);
+          this.todos.splice(index,1);
         }
       }
-    }
+    },
+    // computed: {
+    //   remaining: function(){
+    //     var items =this.todos.filter(function(todo){
+    //     return !todo.isDone;
+    //     });
+    //     return items.length;
+    //   }
+    // }
   });
 })();
